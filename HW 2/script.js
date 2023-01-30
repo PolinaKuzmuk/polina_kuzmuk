ca0a5bdb03bfface0bdb4c55a70a602f17cce061
PolinaKuzmuk <polinka2082@gmail.com>
2023-01-30 13:26:06 +0100

    adding HW 2 Variables

---
+++ HW 2/script.js
@@ -0,0 +1,6 @@
+let year = Number(prompt('Write your birth year (YYYY)'));
+let name = prompt('What is your name?');
+let surname = prompt('What is your sirname?');
+let age = 2023 - year;
+
+alert('User Bio: ' + name + ' ' + surname + ', ' + age + ' years old');
