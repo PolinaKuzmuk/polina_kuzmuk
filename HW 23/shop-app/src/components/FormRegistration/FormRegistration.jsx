4e31a0c870c22f453a9699af0d88dbe0ef627ad3
PolinaKuzmuk <polinka2082@gmail.com>
2023-06-08 17:50:23 +0200

    HW 28 added

--- HW 23/shop-app/src/components/FormRegistration/FormRegistration.jsx
+++ HW 23/shop-app/src/components/FormRegistration/FormRegistration.jsx
@@ -4,41 +4,39 @@ import CustomButton from "../common/Button/Button";
 import API from "../../services/API";
 import { setActiveUSer } from "../../store/userActions";
 import "./FormRegistration.css";
-import { FormControl, TextField, Typography } from "@mui/material";
+import { Formik, Field, Form } from 'formik';
+import { Box, Typography } from "@mui/material";
 import { useDispatch } from "react-redux";
 
 export default function FormRegistration() {
-    const [fullname, setFullname] = useState(false);
     const [userEmail, setUserEmail] = useState(false);
-    const [userPassword, setUserPassword] = useState(false);
-    const [verifyPassword, setVerifyPassword] = useState(false);
     const [isErrorEmail, setErrorEmail] = useState(false);
     const [isErrorPass, setErrorPass] = useState(false);
     const navigate = useNavigate();
     const dispatch = useDispatch();
 
-    function createUserAccount(e) {
-        e.preventDefault();
+    function createUserAccount(values) {
+        setUserEmail(values.email);
         setErrorEmail(false);
         setErrorPass(false);
-        if (userPassword !== verifyPassword) {
+        if (values.password !== values.verifyPassword) {
             setErrorPass(true);
         } else {
             API.getUsers().then(res => {
-                let findDuplicateUser = res.filter(user => user.email === userEmail);
+                let findDuplicateUser = res.filter(user => user.email === values.email);
                 if (findDuplicateUser.length === 0) {
                     const newUser = {
                         orders: [],
                         shoppingCart: [],
-                        name: fullname,
-                        email: userEmail,
-                        password: userPassword,
+                        name: values.fullName,
+                        email: values.email,
+                        password: values.password,
                         status: true
                     };
                     API.createUser(newUser).then(() => {
                         API.getUsers().then(res => {
                             res.filter(user => {
-                                if (user.email.toLowerCase() === userEmail.toLowerCase()) {
+                                if (user.email.toLowerCase() === values.email.toLowerCase()) {
                                     setActiveUSer(dispatch, user);
                                     localStorage.setItem('user', user.id);
                                     navigate('/');
@@ -58,16 +56,28 @@ export default function FormRegistration() {
     }
 
     return (
-        <FormControl className="registration">
+        <Box>
             <Typography variant="h2" className="login-title" fontSize="2rem">Quick registration</Typography>
-            <Typography className="paragraph login-subtitle">For new customers</Typography>
-            <Typography className={`paragraph error ${isErrorEmail ? 'active' : ''}`}>User with email {userEmail} already exist!</Typography>
-            <Typography className={`paragraph error ${isErrorPass ? 'active' : ''}`}>Password not matches!</Typography>
-            <TextField style={style} size="small" type="text" name="full-name" placeholder="Full name" value={fullname || ''} onChange={e => setFullname(e.target.value)} required />
-            <TextField style={style} size="small" type="email" name="email" placeholder="Email Address" value={userEmail || ''} onChange={e => setUserEmail(e.target.value)} required />
-            <TextField style={style} size="small" type="password" name="password" placeholder="Password" value={userPassword || ''} onChange={e => setUserPassword(e.target.value)} required />
-            <TextField style={style} size="small" type="password" name="verify-password" placeholder="Verify password" value={verifyPassword || ''} onChange={e => setVerifyPassword(e.target.value)} required />
-            <CustomButton className="btn btn__register" type="submit" text="Create account" onClick={createUserAccount} />
-        </FormControl>
+            <Typography style={style} className="paragraph login-subtitle">For new customers</Typography>
+            <Typography style={style} className={`paragraph error ${isErrorEmail ? 'active' : ''}`}>User with email {userEmail} already exist!</Typography>
+            <Typography style={style} className={`paragraph error ${isErrorPass ? 'active' : ''}`}>Password not matches!</Typography>
+            <Formik
+                initialValues={{
+                    fullName: "",
+                    email: "",
+                    password: "",
+                    verifyPassword: "",
+                }}
+                onSubmit={(values) => createUserAccount(values)}
+            >
+                <Form className='registration'>
+                    <Field id="fullName" name="fullName" placeholder="Full name" />
+                    <Field id="email" name="email" placeholder="Email Address" />
+                    <Field id="password" name="password" placeholder="Password" />
+                    <Field id="verifyPassword" name="verifyPassword" placeholder="Verify password" />
+                    <CustomButton className="btn btn__register" type="submit" text="Create account" />
+                </Form>
+            </Formik>
+        </Box >
     )
 }
--- HW 23/shop-app/src/components/FormSignIn/FormSignIn.jsx
+++ HW 23/shop-app/src/components/FormSignIn/FormSignIn.jsx
@@ -1,6 +1,7 @@
+import { Formik, Field, Form } from 'formik';
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { FormControl, TextField, Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import CustomButton from "../common/Button/Button";
 import API from "../../services/API";
 import "./FormSignIn.css";
@@ -9,29 +10,30 @@ import { useDispatch } from "react-redux";
 import { setActiveUSer } from "../../store/userActions";
 
 export default function FormSignIn() {
-    const [userEmail, setUserEmail] = useState(false);
-    const [userPassword, setUserPassword] = useState(false);
     const [isErrorEmail, setErrorEmail] = useState(false);
     const [isErrorPass, setErrorPass] = useState(false);
     const navigate = useNavigate();
     const dispatch = useDispatch();
 
-    function signInFunc(e) {
-        e.preventDefault();
+    const style = {
+        'margin': '0 15px 15px 0'
+    }
+
+    function signInFunc(values) {
         setErrorEmail(false);
         setErrorPass(false);
 
         let invalidEmailsList = [];
         API.getUsers().then(res => {
             res.filter(user => {
-                if ((user.email.toLowerCase() === userEmail.toLowerCase()) && (user.password === userPassword)) {
+                if ((user.email.toLowerCase() === values.email.toLowerCase()) && (user.password === values.password)) {
                     API.changeUserStatus(user, true).then(() => {
                         localStorage.setItem('user', user.id);
-                        setActiveUSer(dispatch, {...user, status: true});
+                        setActiveUSer(dispatch, { ...user, status: true });
                         navigate('/');
                     });
                 }
-                else if ((user.email.toLowerCase() === userEmail.toLowerCase()) && (user.password !== userPassword)) {
+                else if ((user.email.toLowerCase() === values.email.toLowerCase()) && (user.password !== values.password)) {
                     setErrorPass(true);
                 } else {
                     invalidEmailsList.push(user);
@@ -44,19 +46,25 @@ export default function FormSignIn() {
         })
     };
 
-    const style = {
-        'margin': '0 15px 15px 0'
-    }
-
     return (
-        <FormControl className="sign-in">
+        <Box>
             <Typography variant="h2" className="login-title" fontSize="2rem">Secure Sign In</Typography>
-            <Typography className="paragraph login-subtitle">For current customers</Typography>
-            <Typography className={`paragraph error ${isErrorEmail ? 'active' : ''}`}>Invalid email.</Typography>
-            <Typography className={`paragraph error ${isErrorPass ? 'active' : ''}`}>Invalid password.</Typography>
-            <TextField style={style} size="small" type="email" name="email" placeholder="Email Address" value={userEmail || ""} onChange={e => setUserEmail(e.target.value)} />
-            <TextField style={style} size="small" type="password" name="password" placeholder="Password" value={userPassword || ""} onChange={e => setUserPassword(e.target.value)} />
-            <CustomButton className="btn btn__sign-in" type="submit" text="Sign in" onClick={signInFunc} />
-        </FormControl>
+            <Typography style={style} className="paragraph login-subtitle">For current customers</Typography>
+            <Typography style={style} className={`paragraph error ${isErrorEmail ? 'active' : ''}`}>Invalid email.</Typography>
+            <Typography style={style} className={`paragraph error ${isErrorPass ? 'active' : ''}`}>Invalid password.</Typography>
+            <Formik
+                initialValues={{
+                    email: "",
+                    password: "",
+                }}
+                onSubmit={(values) => signInFunc(values)}
+            >
+                <Form className='sign-in'>
+                    <Field id="email" name="email" placeholder="Email Address" />
+                    <Field id="password" name="password" placeholder="Password" />
+                    <CustomButton className="btn btn__sign-in" type="submit" text="Sign in" />
+                </Form>
+            </Formik>
+        </Box >
     )
 }
